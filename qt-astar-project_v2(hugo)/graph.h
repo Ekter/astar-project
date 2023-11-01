794e260f66ebc9f5cfe7ad0ca59a99ac7416203e
Ekter <ektermoi1@gmail.com>
2023-11-01 10:20:57 +0100

    feat : modifications to getVertex

--- qt-astar-project_v2(hugo)/graph.h
+++ qt-astar-project_v2(hugo)/graph.h
@@ -15,20 +15,23 @@ class Graph
 public:
     Graph(int size);
     void addVertexToGraph(const Vertex& vertex);
-    //std::vector<Vertex> vertices;
     std::vector<Edge> edges;
 
     void addEdgeToGraph(const u_int32_t vertex1, const u_int32_t vertex2, double distance);
     double distanceBetweenVertices(u_int32_t vertex1, u_int32_t vertex2) const;
     std::vector<u_int32_t> getAdjacentVectors(u_int32_t vertex_id) const {return vertices_[vertex_id].getNeighbours();};
-    Vertex getVertex(u_int32_t v_id) const {return vertices_[v_id];};
+    Vertex& getVertex(u_int32_t v_id) {return vertices_[v_id];};
+    const Vertex& getVertex(u_int32_t v_id) const {return vertices_[v_id];};
     void setEstimate(u_int32_t v_id, double estimate) {estimates_[v_id] = estimate;};
     double getEstimate(u_int32_t v_id) {return estimates_[v_id];};
     std::vector<u_int32_t> bfs(u_int32_t vstart_id, u_int32_t vend_id);
     std::vector<u_int32_t> dijkstra(u_int32_t vstart_id, u_int32_t vend_id);
     std::vector<u_int32_t> astar(u_int32_t vstart_id, u_int32_t vend_id);
+    const std::vector<Vertex> getVertices() const {return vertices_;};
+    int getSize() const {return size_;};
 
 private:
+    int size_;
     std::vector<Vertex> vertices_;
     std::unordered_map<u_int64_t, double> edges_;
     std::vector<double> weights_;
